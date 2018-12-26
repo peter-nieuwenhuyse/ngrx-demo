0a659e7b6c88731a1fc76d09d0a9ed29f4ff53ed
peter nieuwenhuyse <peter.nieuwenhuyse@tvh.com>
2018-12-26 09:43:35 +0100

    added server.js file

---
+++ server.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+const compression = require('compression');
+const helmet = require('helmet');
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.use(compression());
+
+app.get('/health', function(req, res) {
+  res.status(200).send('Health check is OK.');
+});
+
+app.get('/alive', function(req, res) {
+  res.status(200).send('Liveness check is OK.');
+});
+
+app.use(helmet());
+
+app.use(express.static(__dirname + '/www'));
+
+app.enable('trust proxy');
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+app.use(function(req, res) {
+  res.sendFile(path.join(__dirname, '/www', 'index.html'));
+});
+
+app.listen(port, function() {
+  console.log("Server running on port 8080");
+});
