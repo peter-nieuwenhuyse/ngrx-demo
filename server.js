42b2eb70dccb1f94e28e3241d922982b2a28e569
peter nieuwenhuyse <peter.nieuwenhuyse@tvh.com>
2018-12-26 11:01:30 +0100

    try nr 4

--- server.js
+++ server.js
@@ -1,36 +1,14 @@
-'use strict';
-
 const express = require('express');
 const path = require('path');
-const compression = require('compression');
-const helmet = require('helmet');
+
 const app = express();
 
-const port = process.env.PORT || 8080;
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/ngrx-redux'));
 
-app.use(compression());
-
-app.get('/health', function(req, res) {
-  res.status(200).send('Health check is OK.');
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/ngrx-redux/index.html'));
 });
 
-app.get('/alive', function(req, res) {
-  res.status(200).send('Liveness check is OK.');
-});
-
-app.use(helmet());
-
-app.use(express.static(__dirname + '/dist/ngrx-redux/'));
-
-app.enable('trust proxy');
-
-app.get('/', function(req, res){
-  res.render('index');
-});
-app.use(function(req, res) {
-  res.sendFile(path.join(__dirname, '/dist/ngrx-redux/', 'index.html'));
-});
-
-app.listen(port, function() {
-  console.log("Server running");
-});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
